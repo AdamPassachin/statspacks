111b864070c2f1518f025fc35a5cbad87a88997d
Adam Passachin <117946663+AdamPassachin@users.noreply.github.com>
2024-09-10 11:06:01 +0000

    added date in datebar

--- src/components/datebar/Datebar.jsx
+++ src/components/datebar/Datebar.jsx
@@ -3,13 +3,28 @@ import Datebutton from './Datebutton'
 import leftbutton from '/src/assets/leftbutton.svg'
 import rightbutton from '/src/assets/rightbutton.svg'
 import calendar from '/src/assets/calendar.svg'
+import { useState } from 'react'
 
 export default function Datebar(){
+
+    // list of days
+    const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];
+
+    // get index for current day
+    const current_today = () => {
+        const today_index = new Date().getDay();
+        return days[today_index]
+    }
+
+    // state to keep track of current day index
+    const [currentDayIndex, setCurrentDayIndex] = useState(current_today());
+
     return(
         <div className='datebar'>
             <Datebutton orientation = {leftbutton}/>
             <Datebutton orientation = {rightbutton} />
             <img src={calendar} alt = "calendar-image" className='calendar-img'/>
+            <p>Today is {currentDayIndex}</p>
         </div>
     );
 }
