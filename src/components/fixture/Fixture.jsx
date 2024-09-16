e53398a940fedac4cebb01b4efacfb4e6e275df0
Adam Passachin <adampassachin@Adams-MacBook-Pro.local>
2024-09-16 20:29:04 +0200

    added teams

--- src/components/fixture/Fixture.jsx
+++ src/components/fixture/Fixture.jsx
@@ -2,6 +2,8 @@ import './Fixture.css'
 import Fixturebar from './Fixturebar';
 import Fixtureleauge from './Fixtureleauge';
 import leaugelogo from '/src/assets/leaugelogo.svg'
+import image1 from '/src/assets/image1.svg'
+import image2 from '/src/assets/image2.svg'
 
 
 export default function Fixture(){
@@ -11,8 +13,8 @@ export default function Fixture(){
                 <div className='col'>
                     <p className='fixture-header-font'>Fixtures</p>
                     <div className='fixture'>
-                        <Fixtureleauge logo = {leaugelogo} leauge="Premier Leauge" />
-                        <Fixturebar time = "18:00" />
+                        <Fixtureleauge logo = {leaugelogo} leauge="Premier Leauge"/>
+                        <Fixturebar homelogo={image1} awaylogo={image2} homeTeam = "Liverpool" awayTeam = "Manchester United" time = "18:00" />
                     </div>
                 </div>
                 <div className='col'>
--- src/components/fixture/Fixturebar.jsx
+++ src/components/fixture/Fixturebar.jsx
@@ -1,17 +1,24 @@
-import './Fixture.css'
+import './Fixture.css';
 
-export default function Fixturebar({time}){
-    return(
-        <>
-            <div className='row'>
-                <div className='col-2'>
-                    <p className='fixture-font'>{time}</p>
-                </div>
-                <div className='col'>
-                    <div class="vertical-line">
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+export default function Fixturebar({ time, homelogo, awaylogo, homeTeam, awayTeam}) {
+  return (
+    <>
+      <div className='row'>
+        <div className='col-auto'>
+          <p className='fixture-font'>{time}</p>
+        </div>
+        <div className='col-auto'>
+          <div className="vertical-line"></div>
+        </div>
+        <div className='col-auto team-img'>
+          <img src={homelogo} alt='homelogo' className='team-logo' /> 
+          <img src={awaylogo} alt='awaylogo' className='team-logo' /> 
+        </div>
+        <div className='col-auto team-names'>
+            <div><p className='team-font'>{homeTeam}</p> </div>
+            <div><p className='team-font'>{awayTeam}</p> </div>
+        </div>
+      </div>
+    </>
+  );
 }
